83e204d40707c5077db05d2659de71ff964c5c37
Anirudhdhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-07-06 11:14:57 -0400

    Adds second character

--- src/Components/Scene.jsx
+++ src/Components/Scene.jsx
@@ -7,20 +7,14 @@ import { KeyboardControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { Html, Environment } from "@react-three/drei";
-import * as THREE from 'three';
+import * as THREE from "three";
 import Question from "./Question/Question";
+import PointingCharacter from "./PointingCharacter";
 
 const Loader = () => {
   return (
     <Html>
-      <h1
-        style={{
-        /*   color: "white", */
-        alignContent:"center"
-        }}
-      >
-        Loading ...
-      </h1>
+      <h1 style={{ alignContent: "center" }}>Loading ...</h1>
     </Html>
   );
 };
@@ -34,37 +28,36 @@ const keyboardMap = [
 const Scene = () => {
   return (
     <>
-      <KeyboardControls map={keyboardMap} >
-    <div
-      style={{
-        height: "100vh",
-        width: "100vw",
-      }}
-    >
-      <Canvas
-        style={{
-          height: "100vh",
-        }}
-      >
-
-<Suspense fallback={<Loader />}>
-      <Physics >
-      <Environment
-          files="/assets/street.hdr" // Replace with your actual HDRI file path
-          background // Whether to set the environment map as the scene background
-          encoding={THREE.sRGBEncoding} // Optional, for better color rendering
-          intensity={0}
-        />
-        <Hall/>
-        <Camera />
-         <CharacterController/>
-        <Question />
-      </Physics>
-      </Suspense>
-      </Canvas>
- 
-    </div>
-    </KeyboardControls>
+      <KeyboardControls map={keyboardMap}>
+        <div
+          style={{
+            height: "100vh",
+            width: "100vw",
+          }}
+        >
+          <Canvas
+            style={{
+              height: "100vh",
+            }}
+          >
+            <Suspense fallback={<Loader />}>
+              <Physics>
+                <Environment
+                  files="/assets/street.hdr" // Replace with your actual HDRI file path
+                  background // Whether to set the environment map as the scene background
+                  encoding={THREE.sRGBEncoding} // Optional, for better color rendering
+                  intensity={0}
+                />
+                <Hall />
+                <Camera />
+                <CharacterController />
+                <PointingCharacter />
+                <Question />
+              </Physics>
+            </Suspense>
+          </Canvas>
+        </div>
+      </KeyboardControls>
     </>
   );
 };
