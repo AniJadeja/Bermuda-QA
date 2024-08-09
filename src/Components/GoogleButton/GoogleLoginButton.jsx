4afdca4a24a5dc6d047f691121f1c68e586ebb5c
Anirudhdhsinh Jadeja <anirudhdhsinhj@gmail.com>
2024-08-08 22:34:27 -0400

    Configures googleAuth

---
+++ src/Components/GoogleButton/GoogleLoginButton.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+import useUserCookies from "../../hooks/useUserCookies";
+import useAuthStore from "./index";
+
+
+const GoogleLoginButton = () => {
+  const { setCookies } = useUserCookies();
+  const { setLoggedIn } = useAuthStore();
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "fit-content",
+        width: "fit-content",
+        position: "fixed",
+        top: "2%",
+        right: "2%",
+        zIndex: "100",
+      }}
+    >
+      <GoogleLogin
+        useOneTap={true}
+        auto_select
+        shape="circle"
+        onSuccess={(credentialResponse) => {
+          const decoded = jwtDecode(credentialResponse.credential);
+          console.log(decoded);
+          setCookies({
+            user: decoded.email,
+            pname: decoded.name,
+          });
+          setLoggedIn(true);
+        }}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+      />
+    </div>
+  );
+};
+
+export default GoogleLoginButton;
---
+++ src/Components/GoogleButton/GoogleLogoutButton.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { googleLogout } from "@react-oauth/google";
+import useUserCookies from "../../hooks/useUserCookies";
+import useAuthStore from "./index";
+
+const GoogleLogoutButton = () => {
+  const { setCookies } = useUserCookies();
+  const { setLoggedIn } = useAuthStore();
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "fit-content",
+        width: "fit-content",
+        position: "fixed",
+        top: "2%",
+        right: "2%",
+        zIndex: "100",
+        borderRadius: "100px",
+        background: "white",
+        fontSize: "13px",
+      }}
+    >
+      <button
+        style={{
+          paddingRight: "20px",
+          paddingLeft: "20px",
+          borderRadius: "100px",
+        }}
+        onClick={() => {
+          setCookies({
+            user: "",
+            pname: "",
+          });
+          setLoggedIn(false);
+          googleLogout();
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default GoogleLogoutButton;
